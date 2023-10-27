224dd9aaa2126a442a0b8e1f00bdea1175621731
Reggy Shicky <reginahshikanda@gmail.com>
2023-10-27 10:39:13 +0300

    Palindrome Number, reverse a number

---
+++ LeetCode/Palindrome_Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Palindrome_Numbers
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			palidrome_Number palindromeNUm = new palidrome_Number();
+			Console.WriteLine(palindromeNUm.IsPalindrome(124));
+		}
+	}
+}
---
+++ LeetCode/Palindrome_Numbers/palidrome_Number.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Numbers
+{
+	public class palidrome_Number
+	{
+		
+		public bool IsPalindrome(int x)
+		{
+			int OriginalX = x; //store the original number
+			int reverse = 0;
+			int remainder;
+			while (x > 0)
+			{
+				remainder = x % 10;
+				reverse = reverse * 10 + remainder;
+				x = x / 10;
+			}
+			return OriginalX == reverse; //compare the original number with the reversed number
+		}
+		
+
+	}
+}
+
+
+
+//Question: Given an integer x, return true if x is a palindrome and false otherwise
