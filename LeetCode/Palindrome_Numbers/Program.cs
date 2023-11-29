6e5ef323037390ebc4a09b5f95d28b82e9bfb36f
Reggy Shicky <reginahshikanda@gmail.com>
2023-11-29 23:51:47 +0300

    Palindrome strings

--- LeetCode/Palindrome_Numbers/Program.cs
+++ LeetCode/Palindrome_Numbers/Program.cs
@@ -1,11 +1,37 @@
 ﻿namespace Palindrome_Numbers
 {
-	internal class Program
+	public class Program
 	{
 		static void Main(string[] args)
 		{
 			palidrome_Number palindromeNUm = new palidrome_Number();
 			Console.WriteLine(palindromeNUm.IsPalindrome(124));
+			Program program = new Program();
+			Console.WriteLine(program.palindrome("radar"));
+			Console.WriteLine(program.palindrome("radaj"));
+
+        }
+
+		//testing for a palindrome -str
+		public bool palindrome(String str)
+		{
+			str = str.ToLower();
+			int left = 0;
+			int right = str.Length - 1;
+
+			while (left < right)
+			{
+				if (str[left] != str[right])
+				{
+					return false;
+				}
+				else
+				{
+					left++;
+					right--;
+				}
+			}
+			return true;
 		}
 	}
 }
--- LeetCode/Palindrome_Numbers/palidrome_Number.cs
+++ LeetCode/Palindrome_Numbers/palidrome_Number.cs
@@ -30,3 +30,19 @@ namespace Palindrome_Numbers
 
 
 //Question: Given an integer x, return true if x is a palindrome and false otherwise
+
+/*
+ * def palindrome(s: str) -> bool:    #def palindrome(s)
+    left = 0
+    right = len(s) - 1
+    
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
+
+print(palindrome("radar"))
+print(palindrome("radaj"))
+ */
