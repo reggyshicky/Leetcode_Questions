284b1d304048d6edc783895c47f163451199ea5d
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-01 18:19:13 +0300

    Two numbers that add to a target

---
+++ LeetCode/TwoSum_Target/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+
+namespace Nigel;
+
+public class TwoSumTarget
+{
+    public static void Main(string[] args)
+    {
+        TwoSumTarget instance = new TwoSumTarget();
+        int[] myarr = { 1, 2, 3, 4, 5 };
+        Console.WriteLine(instance.TwoSum(myarr, 9));
+    }
+    public bool TwoSum(int[] arr, int target)
+    {
+        int left = 0;
+        int right = arr.Length - 1;
+
+        while (left < right)
+        {
+          
+            if (arr[left] + arr[right] > target)
+            {
+                right--;
+            }
+            else if (arr[right] + arr[left] < target)
+            {
+                left++;
+            }
+            else
+            {
+                return true; //the numbers were found
+            }
+
+        }
+        return false;
+    }
+}
+
+
