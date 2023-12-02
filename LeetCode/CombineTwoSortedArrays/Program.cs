a5708c20d8cf1299ec21eb497396dc48410578fa
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-02 22:58:10 +0300

    Combing two sorted arrays

---
+++ LeetCode/CombineTwoSortedArrays/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CombineTwoSortedArrays
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            int[] arr1 = { 1, 2, 3, 4, 5 };
+            int[] arr2 = { 10, 15, 25, 29 };
+            List<int> results = program.AddTwoSortedArrays(arr1, arr2); 
+
+            foreach(int element in results)
+            {
+                
+                Console.WriteLine(element + " ");
+            }
+        }
+
+        public List<int> AddTwoSortedArrays(int[] arr1, int[] arr2)
+        {
+            List<int> list = new List<int>();
+            int i = 0;
+            int j = 0;
+
+            while (i < arr1.Length && j < arr2.Length)
+            {
+                if (arr1[i] < arr2[j])
+                {
+                    list.Add(arr1[i]);
+                    i++;
+                } 
+                else
+                {
+                    list.Add(arr2[j]);
+                    j++;
+                }
+            }
+            while (i < arr1.Length)
+            {
+                list.Add(arr1[i]);
+                i++;
+            }
+            while (j < arr2.Length)
+            {
+                list.Add(arr2[j]);
+                j++;
+            }
+            return list;
+            
+        } 
+    }
+}
---
+++ LeetCode/pld/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Rodgers;
+
+public class TwoSumTarget
+{
+    public static void Main(string[] args)
+    {
+        TwoSumTarget x = new TwoSumTarget();
+        int[] myarr = { 1, 2, 3, 4, 5, 6 };
+        Console.WriteLine(x.Target(myarr, 50));
+    }
+    public bool Target(int[] arr, int target)
+    {
+        //i have an a sorted array
+        int left = 0;
+        int right = arr.Length - 1;
+
+        int sum = arr[left] + arr[right];
+        while (left < right)
+        {
+            if (sum == target)
+            {
+                return true;
+            }
+            else if (sum > target)
+            {
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+        return false;
+        
+    }
+    
+
+}
