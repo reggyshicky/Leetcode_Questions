b3f6e057d5bfb5dcae8edddb256d41f8ef1392dd
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-04 10:46:15 +0300

    pascal triangle

--- LeetCode/Pascal_Triangle/Program.cs
+++ LeetCode/Pascal_Triangle/Program.cs
@@ -54,7 +54,7 @@ Row 2 has 3 elements.
 Row 3 has 4 elements.
 And so on. This pattern is represented by the loop condition y <= x. When x is 0, the loop will iterate only once (for y = 0). When x is 1, the loop will iterate twice (for y = 0 and y = 1), and so on.
 
-The purpose of this loop is to iterate over the elements in the current row (ct) and calculate their values based on the conditions specified in the if-else block. It ensures that the loop iterates over the correct range of indices for the current row in Pascal's Triangle.
+The purpose of this loop is to iterate over the elements in the current row (list) and calculate their values based on the conditions specified in the if-else block. It ensures that the loop iterates over the correct range of indices for the current row in Pascal's Triangle.
 
 So, in summary, the condition y <= x ensures that the loop iterates over the valid indices for the current row, where y varies from 0 to the row number (x).
  */
