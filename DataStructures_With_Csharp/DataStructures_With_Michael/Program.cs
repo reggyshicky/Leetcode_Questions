e76a82b0b477a785da91fad4c0ff26fc3778aded
Priscah Reginah Shikanda <reginahshikanda@gmail.com>
2025-02-10 23:25:52 +0300

    double.NegativeInfinity , double.PositiveInfinity

---
+++ DataStructures_With_Csharp/DataStructures_With_Michael/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace DataStructure;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Program p = new Program();
+        p.WriteName();
+
+        Console.WriteLine(ReturnNumber(90));
+
+        int[] arr = { -10, -1000 };
+        // var num = double.NegativeInfinity; //covers the lowest lowest values, double.PositiveInfinity covers all the max max values
+        int num = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] > num)
+            {
+                num = arr[i];
+            }
+        }
+        Console.WriteLine("my num " + num);
+
+        Console.WriteLine("my other num is " + arr.Max());
+
+    }
+
+    public void WriteName()
+    {
+        Console.WriteLine("His name Michael and he eats a lot!");
+    }
+
+    public static int ReturnNumber(int num)
+    {
+        return num;
+    }
+}
+
+
+
+
